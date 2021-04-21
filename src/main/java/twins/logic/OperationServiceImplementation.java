bddea8ee14aaed785397a4ac41e01ab7e61fe73c
katya boyko <katya.boyko@s.afeka.ac.il>
2021-04-21 16:48:05 +0000

    OperationServiceImplementation.java edited online with Bitbucket

--- src/main/java/twins/logic/OperationServiceImplementation.java
+++ src/main/java/twins/logic/OperationServiceImplementation.java
@@ -8,7 +8,7 @@ import java.util.List;
 //import java.util.UUID;
 
 //import javax.annotation.PostConstruct;
-
+//test pull
 import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
